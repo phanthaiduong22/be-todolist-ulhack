447cd51d3e95a32e0f807818b1cab8605e78013c
Duong Phan <phanthaiduong2000@gmail.com>
2021-08-14 13:35:08 +0700

    merge

--- db.sql
+++ db.sql
@@ -20,12 +20,12 @@ DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE `tasks` (
   `task_id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  -- `section_id` int NOT NULL,
+  `section_id` int NOT NULL,
   `task_name` varchar(50)  NOT NULL,
   `is_importance` BOOLEAN,
   `is_done` BOOLEAN DEFAULT FALSE,
   `due_date` varchar(50) NOT NULL,
   FOREIGN KEY (`username`) REFERENCES users(`username`),
-  -- FOREIGN KEY (`section_id`) REFERENCES sections(`section_id`),
+  FOREIGN KEY (`section_id`) REFERENCES sections(`section_id`),
   PRIMARY KEY (`task_id`)
 );
